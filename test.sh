aaf5d054eea16b67fa0e23d09b639f2f0ac66eee
Ahmed Khalid <ahmed.khalid.ali1@gmail.com>
2021-11-22 16:15:04 +0200

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
-pkg install git 
-cd 
-git clone https://github.com/ahmedKhalid1/su-init
-su 
-cp su-init/bash /system/bin/bash
+cp bash /system/bin/bash
 chmod 755 /system/bin/bash
 exit 
 alias su='su -c bash'
