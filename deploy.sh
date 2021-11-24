97fc2de67572dd7db15e8b4f7d663e556d4bae0b
Ahmed Khalid <ahmed.khalid.ali1@gmail.com>
2021-11-24 15:08:26 +0200

    Update and rename test.sh to deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cp bash /system/bin/bash
+chmod 755 /system/bin/bash
+cp bashrc /data/data/com.termux/files/home/.bashrc
+chmod 777 /data/data/com.termux/files/home/.bashrc
--- test.sh
+++
@@ -1,4 +0,0 @@
-cp bash /system/bin/bash
-chmod 755 /system/bin/bash
-cp .bashrc /data/data/com.termux/files/home
-chmod 777 /data/data/com.termux/files/home
